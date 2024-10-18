80050b745016db69484261edccc9e448656bc769
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2024-10-18 14:03:30 +0300

    database online book store

---
+++ alx_book_store.sql
@@ -0,0 +1,34 @@
+use alx_book_store;
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,  -- Specify the data type for customer_id
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT 
+);
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+CREATE TABLE Order_Details (
+    order_detailid INT PRIMARY KEY,  
+    order_id INT,
+    book_id INT,
+    quantity FLOAT,  
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
+
+create table Authors(
+    author_id INT Primary key,
+    author_name VARCHAR(215)
+);
+create table Books(
+    book_id int PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    Foreign Key (author_id) REFERENCES Authors(author_id),
+    price DECIMAL,
+    publication_date DATE
+);
